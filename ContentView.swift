d86c0d95d0a33e901c8f731ddb0d2fd5dc6d47cf
Peterboii <pswanson8432@stu.d214.org>
2025-04-17 11:27:00 -0700

    Added a bracket.

--- ContentView.swift
+++ ContentView.swift
@@ -4,45 +4,47 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Advanced Car Creator")
-       VStack {
-          Text("Advanced Car Creator")
+            VStack {
+                Text("Advanced Car Creator")
                     .font(.largeTitle)
                     .padding()
-            Divider()
-            Text("Create New Car Information")
-                    .font(.title3)
-            NavigationStack {
-                NavigationLink {
-                    NewCarInfomationView()
-                } label: {
-                    Image(systemName: "car.badge.gearshape")
-                }
-                .padding()
-                Spacer()
-           
-                Text("Design New Car Look")
-                    .foregroundStyle(.red)
-                    .font(.system(size: 25, design: .monospaced))
-               
-                    
-           Divider()
-                
+                Divider()
                 Text("Create New Car Information")
                     .font(.title3)
-            NavigationStack {
-                NavigationLink {
-                    NewCarInfomationView()
-                } label: {
-                    Image(systemName: "car.badge.gearshape")
+                NavigationStack {
+                    NavigationLink {
+                        NewCarInfomationView()
+                    } label: {
+                        Image(systemName: "car.badge.gearshape")
+                    }
+                    .padding()
+                    Spacer()
+                    
+                    Text("Design New Car Look")
+                        .foregroundStyle(.red)
+                        .font(.system(size: 25, design: .monospaced))
+                    
+                    
+                    Divider()
+                    
+                    Text("Create New Car Information")
+                        .font(.title3)
+                    NavigationStack {
+                        NavigationLink {
+                            NewCarInfomationView()
+                        } label: {
+                            Image(systemName: "car.badge.gearshape")
+                        }
+                        
+                        Spacer()
+                        
+                        Divider()
+                        Spacer()
+                    }
+                    
                 }
-                
-                Spacer()
-                
-                Divider()
-                Spacer()
             }
             
         }
     }
-    
 }
