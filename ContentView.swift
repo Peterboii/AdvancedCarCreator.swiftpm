a78fbd6cac35f608d6e678c1b4f0a7b938a117d3
Peterboii <pswanson8432@stu.d214.org>
2025-04-17 11:05:49 -0700

    Added a navigation stack and link.

--- ContentView.swift
+++ ContentView.swift
@@ -3,8 +3,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
       Text("Advanced Car Creator")
+         
             
-        
+        NavigationStack {
+            NavigationLink {
+                NewCarInfomationView()
+            } label: {
+                Image(systemName: "figure.archery")
+            }
+        }
       }
     }
 
