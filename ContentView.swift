49cea4c02ca0e6af6b7183fb36871ddfff363668
Peterboii <pswanson8432@stu.d214.org>
2025-04-17 10:37:40 -0700

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      Text("Advanced Car Creator")
+            
+        Text("Create new car information")
+        NavigationStack {
+            NavigationLink {
+                
+            }
+        }
+    }
+}
