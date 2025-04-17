79a92c4bc790d87387a8de704056d51d3f207ccd
veerlol123 <vpatel8509@stu.d214.org>
2025-04-17 13:23:12 -0500

    added divider & Spacer

--- ContentView.swift
+++ ContentView.swift
@@ -2,25 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      Text("Advanced Car Creator")
-         
-            
-        NavigationStack {
-            NavigationLink {
-                NewCarInfomationView()
-            } label: {
-                Image(systemName: "figure.archery")
-            }
-        VStack {
-            Text("Advanced Car Creator")
-                .font(.largeTitle)
-                .padding()
+       VStack {
+          Text("Advanced Car Creator")
+                    .font(.largeTitle)
+                    .padding()
            
-            Text("Create New Car Information")
-                .font(.title3)
-            Spacer()
+           Divider()
+                
+                Text("Create New Car Information")
+                    .font(.title3)
+            NavigationStack {
+                NavigationLink {
+                    NewCarInfomationView()
+                } label: {
+                    Image(systemName: "car.badge.gearshape")
+                }
+                
+                Spacer()
+                
+                Divider()
+                Spacer()
+            }
+            
         }
-        
-      }
     }
-
+    
+}
