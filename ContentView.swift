01be6e22951810df875b07484df4a460d41e46dd
Peterboii <pswanson8432@stu.d214.org>
2025-04-17 10:44:34 -0700

    i created a text called New Car Information.

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,7 @@ struct ContentView: View {
     var body: some View {
       Text("Advanced Car Creator")
             
-        Text("Create new car information")
-        NavigationStack {
-            NavigationLink {
-                
-            }
-        }
+        
+      }
     }
-}
+
