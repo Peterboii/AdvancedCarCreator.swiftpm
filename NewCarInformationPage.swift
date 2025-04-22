09ffe6dc8a198993f33b6a976104d9174f5a2e3e
Peterboii <pswanson8432@stu.d214.org>
2025-04-22 11:19:39 -0700

    I changed the Int varaible into Int optional variables.

--- NewCarInformationPage.swift
+++ NewCarInformationPage.swift
@@ -1,14 +1,16 @@
 import SwiftUI
 
 struct NewCarInfomationPage: View {
-    @State var hp = 0
-    @State var engine = 0
-    @State var weight = 0
+    @State var hp: Int?
+    @State var engine: Int?
+    @State var weight: Int?
     var body: some View {
         Text("Hosrepower")
+            
         Text("Engine")
             .font(.largeTitle)
         TextField("Enter Horsepower Amount (000-1800)", value: $hp, format: .number)
+            
         
         TextField("Enter Engine Cylinder Number", value: $engine, format: .number)
             .textFieldStyle(.roundedBorder)
