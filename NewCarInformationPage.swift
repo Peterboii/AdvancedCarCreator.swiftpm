98853d4f88a7b9e5328b0c76086fc98f4b298e04
veerlol123 <vpatel8509@stu.d214.org>
2025-04-22 13:14:34 -0500

    Changed datatype to int

--- NewCarInformationPage.swift
+++ NewCarInformationPage.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 struct NewCarInfomationPage: View {
-    @State var hp = ""
-    @State var engine = ""
-    @State var weight = ""
+    @State var hp = 0
+    @State var engine = 0
+    @State var weight = 0
     var body: some View {
         Text("Hosrepower")
         Text("Engine")
             .font(.largeTitle)
-        TextField("Enter Horsepower Amount (000-1800)", text: $hp)
+        TextField("Enter Horsepower Amount (000-1800)", value: $hp, format: .number)
         
         TextField("Enter Engine Cylinder Number", value: $engine, format: .number)
             .textFieldStyle(.roundedBorder)
