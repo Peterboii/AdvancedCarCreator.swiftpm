bfa6d7b944fe192e37154d800ae8e67e58dccb32
Peterboii <pswanson8432@stu.d214.org>
2025-04-22 10:29:03 -0700

    I moved some code around to imporve presentation.

--- ContentView.swift
+++ ContentView.swift
@@ -3,48 +3,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Advanced Car Creator")
-            VStack {
                 Text("Advanced Car Creator")
                     .font(.largeTitle)
                     .padding()
                 Divider()
                 Text("Create New Car Information")
                     .font(.title3)
+            NavigationStack {
+                NavigationLink {
+                    NewCarInfomationView()
+                } label: {
+                    Image(systemName: "car.badge.gearshape")
+                }
+                .padding()
+                Spacer()
+                Divider()
+                    .padding()
+                Text("Design New Car Look")
+                    .foregroundStyle(.red)
+                    .font(.system(size: 25, design: .monospaced))
+                    .padding()
                 NavigationStack {
                     NavigationLink {
-                        NewCarInfomationView()
+                        DesignNewCarView()
                     } label: {
-                        Image(systemName: "car.badge.gearshape")
+                        Image(systemName: "car.side")
                     }
-                    .padding()
+                }
                     Spacer()
-                    
-                    Text("Design New Car Look")
-                        .foregroundStyle(.red)
-                        .font(.system(size: 25, design: .monospaced))
-                    
-                    
-                    Divider()
-                    
-                    Text("Create New Car Information")
-                        .font(.title3)
-                    NavigationStack {
-                        NavigationLink {
-                            NewCarInfomationView()
-                        } label: {
-                            Image(systemName: "car.badge.gearshape")
-                        }
-                        
-                        Spacer()
-                        
-                        Divider()
-                        Spacer()
+                    Spacer()
                     }
                     
                 }
             }
             
         }
-    }
-}
+    
+
---
+++ DesignNewCarPage.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct DesignNewCarView: View {
+    var body: some View {
+        Text("DF")
+    }
+}
+
