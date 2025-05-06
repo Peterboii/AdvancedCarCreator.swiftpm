93e9c859e90cd59adf51f43b5d41c293c9bb60f6
Peterboii <pswanson8432@stu.d214.org>
2025-05-06 11:39:34 -0700

    Intillazied CanvasView into "DesignNewCarPage" view to have it work and be displayed.

--- DesignNewCarPage.swift
+++ DesignNewCarPage.swift
@@ -1,16 +1,28 @@
 import SwiftUI
 import PencilKit
+
 struct DesignNewCarPage: View {
-    
     var body: some View {
         Text("Car Design")
             .font(.largeTitle)
             .padding()
-            
-        
-
-        
     }
+}
+
+struct DrawingView: View {
+    @State var canvasView = PKCanvasView()
     
+    var body: some View {
+        VStack {
+            CanvasView(canvasData: $canvasView)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Button("Clear Drawing") {
+                canvasView.drawing = PKDrawing()
+            }
+            .padding()
+        }
+        .navigationBarTitle("Draw New Car", displayMode: .inline)
+    }
 }
 
