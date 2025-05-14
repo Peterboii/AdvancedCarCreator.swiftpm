7cb2ae8848c9aafff9fe6c3112fe6033e8f8f8b8
Peterboii <pswanson8432@stu.d214.org>
2025-05-14 10:59:13 -0700

    Added PreviewCanvas, a HStack, and a @State variable.

--- DisplayView.swift
+++ DisplayView.swift
@@ -2,16 +2,17 @@ import SwiftUI
 
 struct DisplayView: View {
 @Binding var ZeroToSixtyCalculator: Double
-    @Binding var TorqueCalculator: Double
-
-    @Binding var topSpeedCalculator: Double
+@Binding var TorqueCalculator: Double
+@Binding var topSpeedCalculator: Double
+@State var drawing = PKDrawing()
     var body: some View {
-         
-        VStack(spacing: 40) {
-            Text("\(ZeroToSixtyCalculator) Seconds")
-            Text("\(TorqueCalculator) lbs")
-            Text("\(topSpeedCalculator) mph")
-            
+        HStack {
+            VStack(spacing: 40) {
+                Text("\(ZeroToSixtyCalculator) Seconds")
+                Text("\(TorqueCalculator) lbs")
+                Text("\(topSpeedCalculator) mph")
+                PreviewCanvas(drawing: drawing)
+            }
         }
     }
 }
