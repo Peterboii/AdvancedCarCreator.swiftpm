230206982d35cea6b9f7020764a04a4b469381e6
veerlol123 <vpatel8509@stu.d214.org>
2025-05-14 12:59:44 -0500

    imported pencil kit. added vstack on top of texts

--- DisplayView.swift
+++ DisplayView.swift
@@ -1,13 +1,16 @@
 import SwiftUI
-
+import PencilKit
 struct DisplayView: View {
 @Binding var ZeroToSixtyCalculator: Double
     @Binding var TorqueCalculator: Double
 
     @Binding var topSpeedCalculator: Double
     var body: some View {
-         
+         VStack {
+            
+        }
         VStack(spacing: 40) {
+            
             Text("\(ZeroToSixtyCalculator) Seconds")
             Text("\(TorqueCalculator) lbs")
             Text("\(topSpeedCalculator) mph")
