12fbd64708ceb8275c99629dfed73c734b076f59
Peterboii <pswanson8432@stu.d214.org>
2025-05-14 10:39:07 -0700

    Added two texts and Binding Variables.

--- DisplayView.swift
+++ DisplayView.swift
@@ -1,8 +1,16 @@
 import SwiftUI
 
 struct DisplayView: View {
+@Binding var ZeroToSixtyCalculator: Double
+    @Binding var TorqueCalculator: Double
+
     var body: some View {
-        
+        VStack(spacing: 40) {
+            Text("\(ZeroToSixtyCalculator) Seconds")
+            Text("\(TorqueCalculator) lbs")
+            
+            
+        }
     }
 }
 
