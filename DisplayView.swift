8a550741a20b90240e983e10bae83c15d2b0ae22
veerlol123 <vpatel8509@stu.d214.org>
2025-05-14 12:40:24 -0500

    added binding var as double. added text displayin the var

--- DisplayView.swift
+++ DisplayView.swift
@@ -1,8 +1,9 @@
 import SwiftUI
 
 struct DisplayView: View {
+    @Binding var topSpeedCalculator: Double
     var body: some View {
-        
+         Text("\(topSpeedCalculator) mph")
     }
 }
 
