d1f38d0557ffb80818d979fc50c4afc0bb73bcfa
veerlol123 <vpatel8509@stu.d214.org>
2025-05-20 13:33:19 -0500

    added return bounds.isEmpty ? nil : canvasView.drawing.image(from: bounds, scale: 1.0). }

--- DrawingToImageActivation.swift
+++ DrawingToImageActivation.swift
@@ -5,6 +5,6 @@ class CanvasData: ObservableObject {
 @Published var canvasView = PKCanvasView()
     
     var drawingImage: UIImage? {
-        
+        return bounds.isEmpty ? nil : canvasView.drawing.image(from: bounds, scale: 1.0). }
     }
-}
+
