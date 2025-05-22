38ff5bd2b991496e662b453f0a8f1805e753e464
Peterboii <pswanson8432@stu.d214.org>
2025-05-22 11:31:13 -0700

    Changed from turing darwing into image, to turning it into a PNG

--- DrawingToImageActivation.swift
+++ DrawingToImageActivation.swift
@@ -7,12 +7,21 @@ class CanvasData: ObservableObject {
     var drawingImage: UIImage? {
         let bounds = canvasView.drawing.bounds
         
-        return bounds.isEmpty ? nil : canvasView.drawing.image(from: bounds, scale: 1.0).
+        return bounds.isEmpty ? nil : canvasView.drawing.image(from: bounds, scale: 1.0)
+    }
+    
+    func clear() {
+        canvasView.drawing = PKDrawing()
+    }
+    
+    func exportPNGData() -> Data? {
+        guard let image = drawingImage else { return nil }
+        return image.pngData()
     }
 }
 
 struct CanvasView: UIViewRepresentable {
-    @ObservedObject var canvasData = CanvasData()
+    @ObservedObject var canvasData = CanvasData
     
     
     func makeUIView(context: Context) -> PKCanvasView {
