b3d4e2868f4cec751e187e8f4964be7444c1a2d3
veerlol123 <vpatel8509@stu.d214.org>
2025-05-22 13:30:13 -0500

    fixed error

--- DrawingToImageActivation.swift
+++ DrawingToImageActivation.swift
@@ -7,17 +7,17 @@ class CanvasData: ObservableObject {
     var drawingImage: UIImage? {
         let bounds = canvasView.drawing.bounds
         
-        return bounds.isEmpty ? nil : canvasView.drawing.image(from: bounds, scale: 1.0).
+        return bounds.isEmpty ? nil : canvasView.drawing.image(from: bounds, scale: 1.0)
     }
 }
 
-struct CanvasView: UIViewRepresentable {
-    @ObservedObject var canvasData = CanvasData()
+struct canvasView: UIViewRepresentable {
+    @ObservedObject var canvasData = CanvasData
     
     
     func makeUIView(context: Context) -> PKCanvasView {
         canvasData.canvasView.drawingPolicy = .anyInput
-       
+        canvasData.canvasView.tool = PKInkingTool(.pen, color: .black, width: 5)
         return canvasData.canvasView
     }
     func updateUIView(_ uiView: PKCanvasView, context: Context) {
